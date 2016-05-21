56ee4d73de07bdb21e38286a29d3ec1858482064
mistler <mistler@mail.ru>
2016-05-21 10:44:51 +0600

    add href to index page from all other pages

--- app/controllers/LibraryController.java
+++ app/controllers/LibraryController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import com.avaje.ebean.Model;
 import models.book.Book;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
+
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -11,12 +15,39 @@ import play.mvc.Result;
  */
 public class LibraryController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    public Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
 
+    public Result booksPage() {
+        List<Book> books = new Model.Finder(String.class, Book.class).all();
+        return ok(books_page.render(books));
+    }
+
+    public Result instancesPage() {
+        List<Book> books = new Model.Finder(String.class, Book.class).all();
+        int i = 0;
+        return ok(instances_page.render(books, i));
+    }
+
+    public Result usersPage() {
+        return ok(users_page.render());
+    }
+
+    public Result deliveryPointsPage() {
+        return ok(delivery_points_page.render());
+    }
+
+    public Result transfersPage() {
+        return ok(transfers_page.render());
+    }
+
+    public Result addBook() {
+
+        Book book = Form.form(Book.class).bindFromRequest().get();
+        book.save();
+
+        return redirect(routes.LibraryController.booksPage());
+    }
 
 }
